6422e96b41e69e5895539b7c5da81b20d2e96c6c
jayant <jayanttripathy1985@gmail.com>
2024-08-15 23:58:20 +0530

    Add project files.

---
+++ DotNet8Auth-Identity/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet8Auth_Identity.Data
+{
+    public class DataContext: IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+    }
+}
